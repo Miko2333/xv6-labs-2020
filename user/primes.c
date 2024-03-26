8352be36279c87e7c4467228a951571bae8fd440
Miko2333 <liuxirui03@hotmail.com>
2024-03-26 22:14:16 +0800

    Corrected primes.c

--- user/primes.c
+++ user/primes.c
@@ -9,52 +9,47 @@ int main(int argc, char *argv[]){
     }
     int p[2];
     pipe(p);
-    if(fork() == 0){
-        int p_read = p[0], p_write = -1;
-        int now = 0, num = 0;
-        while(1){
-            int siz = read(p_read, &now, sizeof(now));
-            if(siz == 0) break;
-            if(num == 0){
-                num = now;
-                printf("prime: %d\n", num);
-                continue;
-            }
-            else if(now % num == 0){
-                continue;
-            }
-            if(p_write < 0){
-                pipe(p);
-                if(fork() == 0){ // only fork once for a process
-                    p_read = p[0];
-                }
-                else{
-                    p_write = p[1];
-                }
-            }
-            if(p_write >= 0){
-                write(p_write, &now, sizeof(now));
-            }
-        }
-        if(p_write >= 0){
-            wait(0);
-        }
-        if(p_read >= 0){
-            close(p_read);
-        }
-        if(p_write >= 0){
-            close(p_write);
-        }
-        exit(0);
+    for(int i = 2; i <= 35; i++){
+        //printf("write %d\n", i);
+        write(p[1], &i, sizeof(i));
     }
-    else{
-        int i;
-        for(i = 2; i < 35; i++){
-            //printf("write %d\n", i);
-            write(p[1], &i, sizeof(i));
+    close(p[1]);
+    int p_read = p[0], p_write = -1;
+    int now = 0, num = 0;
+    int forked = 0;
+    while(1){
+        int siz = read(p_read, &now, sizeof(now));
+        if(siz == 0) break;
+        if(num == 0){
+            num = now;
+            printf("prime %d\n", num);
+            continue;
         }
-        close(p[1]);
+        else if(now % num == 0){
+            continue;
+        }
+        if(!forked){    
+            pipe(p);
+            if(fork() == 0){ // only fork once for a process
+                close(p[1]);
+                close(p_read);
+                p_read = p[0];
+                p_write = -1;
+                num = 0;
+            }
+            else{
+                close(p[0]);
+                p_write = p[1];
+                forked = 1;
+            }
+        }
+        if(p_write >= 0)
+            write(p_write, &now, sizeof(now));
+    }
+    if(forked){
+        close(p_write);
         wait(0);
     }
+    close(p_read);
     exit(0);
 }
